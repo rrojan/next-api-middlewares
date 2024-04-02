fc60e63ea66c55abf2c66ae1eb196c1ec61d9ca4
Rojan Rajbhandari <rrojan20@gmail.com>
2024-04-02 14:47:08 +0545

    chore: build latest 0.1.1

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -2,10 +2,13 @@ import * as next_server from 'next/server';
 import { NextRequest, NextResponse } from 'next/server';
 
 type NextFn = (pipeParams?: any) => Promise<NextResponse | void>;
-type Params = Record<string, any>;
+type Params = {
+    params?: Record<string, string>;
+    pipeParams?: any;
+};
 type MiddlewareOrHandler<AdditionalReqProperties = void> = (req: NextRequest & AdditionalReqProperties, params?: Params, next?: NextFn) => NextResponse | Promise<NextResponse | void>;
 type MiddlewareChain = MiddlewareOrHandler<any>[];
 
-declare const pipe: (...fns: MiddlewareChain) => Promise<(req: NextRequest, params: any) => Promise<void | next_server.NextResponse<unknown>>>;
+declare const pipe: (...fns: MiddlewareChain) => Promise<(req: NextRequest, params: Params) => Promise<void | next_server.NextResponse<unknown>>>;
 
 export { pipe as default };
